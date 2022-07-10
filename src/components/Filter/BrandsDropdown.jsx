bfa54dd22cf346ac51e72944a9d1ec7439f9176c
OndrejBurval <ondrej.burval@outlook.cz>
2022-07-10 11:24:05 +0200

    Search ladění

--- src/components/Filter/BrandsDropdown.jsx
+++ src/components/Filter/BrandsDropdown.jsx
@@ -1,10 +1,18 @@
 import { useState, useEffect, useRef } from "react";
+import { Dropdown, Input } from "rsuite";
 import styled from "styled-components";
 
 export default function BrandsDropdown({ setBrand, brand }){
     const selectInput = useRef()
     const [brands, setBrands] = useState([])
     const [limit, setLimit] = useState(1000)
+    const [search, setSearch] = useState()
+
+    // const searchInput = useRef()
+    // const searchFunc = (a) => {
+    //     if (a.toLowerCase().includes( searchInput.current.value.toLowerCase() )) return a
+    // }
+
 
     useEffect(() => {
         fetch("https://srovnavac-backend.herokuapp.com/getAllBrands")
@@ -13,16 +21,19 @@ export default function BrandsDropdown({ setBrand, brand }){
     }, [])
 
 
+
     const listBrands =  brands.map((brand, key) => {
         return (
-            //<Dropdown.Item as="button" key={key} onClick={event => setBrand(brand)}> { brand } </Dropdown.Item>
-            <option value={ brand } key={ key }> { brand }</option>
+            // <Dropdown.Item key={key} onClick={event => setBrand(brand)}> { brand } </Dropdown.Item>
+            <option value={ brand } key={ key } selected={key === 1 && true}> { brand }</option>
         )
     })
 
     const onChange = () => {
         setBrand(selectInput.current.value)
     }
+
+
 
     const btnText = brand ? brand : "Všechny značky"
 
@@ -33,18 +44,17 @@ export default function BrandsDropdown({ setBrand, brand }){
         </Select>
 
 
-        // <Dropdown title={ btnText } trigger='click' style={styles}>
+        // <Dropdown title={ btnText } trigger='click'>
+        //     <Input placeholder="Vyhleda..." ref={ searchInput } onChange={ () => setSearch(searchInput.current.value) } />
         //     { listBrands }
-        //     <Button onClick={event => setLimit(oldValue => oldValue + 100)}> Více + </Button>
         // </Dropdown>
     )
 }
 
 const Select = styled.select`
-  background: linear-gradient(to right, #009FE3, #00CADD);
+  border: solid #009FE3 2px;
   font-weight: bold;
   padding: 10px 15px;
   border-radius: 10px;
-  border: none;
   box-shadow: rgb(0 0 0 / 10%) 0px 8px 16px;
 `
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,43 +1,48 @@
 import { motion } from "framer-motion";
 import styled from "styled-components";
+import placeholder from "../assets/placeholder-image.png"
 
 export default function Product({title, brand, link, objectID, imageLink, desc, price}){
 
+    const onError = (e) => e.currentTarget.src = placeholder
+
+    const includesPrefix = link.includes("http")
+
 
     return (
-
             <Card
+                whileHover={{ scale: 1.025 }}
                 initial={{ y: -10, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
                 exit={{ y: -10, opacity: 0 }}
                 transition={{ type: "spring" }}
+                href={ includesPrefix ? link  : (`https://${link}`) }
+                target="_blank"
             >
-                <span>
+                <Brand>
                     { brand.toUpperCase() }
-                </span>
-                <h3>
-                    <a href={ link } target="_blank" rel="noopener noreferrer">
-                        { title.substring(0, 25) + "..." }
-                    </a>
-                </h3>
+                </Brand>
+                <Title>
+                    { title.substring(0, 25) + "..." }
+                </Title>
+
                 <img id={ objectID }
                      src={ imageLink }
                      alt={ title.substring(0, title.length - 1) }
-                     width="70px"
-                     height="70px"
+                     onError={ onError }
                 />
 
-                <strong>
+                <Description>
                     { desc.substring(0, 35) + "..." }
-                </strong>
-                <p>
+                </Description>
+                <Price>
                 { price } CZK
-                </p>
+                </Price>
             </Card>
     )
 }
 
-const Card = styled(motion.div)`
+const Card = styled(motion.a)`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -46,4 +51,35 @@ const Card = styled(motion.div)`
   box-shadow: rgb(0 0 0 / 10%) 0px 8px 16px;
   border-radius: 12px;
   padding: 10px;
+  
+  *{
+    text-decoration: none;
+    color: black;
+  }
+
+  &:hover, &:focus{
+    text-decoration: none;
+
+  }
+  
+  img{
+    height: 70px;
+  }
+`
+
+const Brand = styled.p`
+
+`
+
+const Title = styled.h3`
+  color: #212121;
+  font-size: 1rem !important;
+`
+
+const Description = styled.p``
+
+const Price = styled.h3`
+  font-weight: 900;
+  color: #009fe3;
+  font-size: 1.15rem;
 `
