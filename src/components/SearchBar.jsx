399b20195164bda2bb98f41b8a60006c93a1f082
Ondřej Buřval <89531377+OndrejBurval@users.noreply.github.com>
2022-07-12 14:42:09 +0200

    ladění searche

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,7 @@
 import { InputGroup, Input, Checkbox } from "rsuite";
-import {useRef} from "react";
+import { useRef } from "react";
+import styled from "styled-components";
+import {Search} from "react-bootstrap-icons";
 
 export default function SearchBar({setSearch, setLiveSearch, liveSearch}){
 
@@ -8,32 +10,76 @@ export default function SearchBar({setSearch, setLiveSearch, liveSearch}){
 
     const inputSearch = useRef()
 
-    const styles = {
-        maxWidth: "800px",
-        marginInline: "auto",
-        paddingBlock: "50px"
-    }
 
     const onChange = () => {
         setSearch(inputSearch.current.value)
     }
 
     return(
-        <div className="searchBar" style={styles}>
+        <Wrapper>
             <h2>
                 Vybírejte z tisíců produktů
             </h2>
             <form className="searchForm">
                 <InputGroup>
                     <Input type="text" name="search" ref={ inputSearch } placeholder="Např. šlapací kára" defaultValue={ defaultValue } onChange={ onChange } />
-                    <Checkbox onChange={ event => setLiveSearch(oldValue => !oldValue) } defaultChecked={ liveSearch }>
+                    <Checkbox onChange={ () => setLiveSearch(oldValue => !oldValue) } defaultChecked={ liveSearch }>
                         Live search
                     </Checkbox>
-                    <InputGroup.Button type="submit">
+                    <InputGroup.Button type="submit" appearance="secondary">
+                        <Search size={ 19 } color="#009fe3" />
                         Vyhledat
                     </InputGroup.Button>
                 </InputGroup>
             </form>
-        </div>
+        </Wrapper>
     )
 }
+
+const Wrapper = styled.div`
+  max-width: 800px;
+  margin-inline: auto;
+  padding-block: 50px;
+  
+  form, .rs-input-group{
+    border-radius: 15px !important;
+  }
+  
+  
+  input{
+    border-top-left-radius: 15px !important;
+    border-bottom-left-radius: 15px !important;
+  }
+  
+  .rs-checkbox-checker label{
+    span{
+      position: relative;
+      top: 0;
+      left: 0;
+    }
+    display: grid;
+    grid-auto-flow: column;
+    place-items: center;
+  }
+
+  button{
+    border-top-right-radius: 15px !important;
+    border-bottom-right-radius: 15px !important;
+    background: #00cadd;
+    color: white;
+    font-size: 1rem;
+    
+    svg{
+      transition: .3s;
+      margin-right: 10px;
+    }
+    
+    &:hover{
+      background: #00a1e3;
+      
+      svg{
+        fill: white !important;
+      }
+    }
+  }
+`
